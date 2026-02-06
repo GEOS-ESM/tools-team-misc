7572f1b433cbc462e179ecc6276c638535586076
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2026-02-05 19:56:54 -0500

    Added auto-detect to test.sh.

--- projects/EarthNow/bin/test.sh
+++ projects/EarthNow/bin/test.sh
@@ -4,12 +4,26 @@ source /usr/share/lmod/lmod/init/bash
 module load python/GEOSpyD
 module load ffmpeg
 
+bindir=`dirname $0`
+cd $bindir
+bindir=`pwd`
+rootdir=`dirname $bindir`
+srcdir=$rootdir/src
+
+if [ -z "$PYTHONPATH" ]; then
+  export PYTHONPATH=$srcdir
+else
+  export PYTHONPATH=${PYTHONPATH}:$srcdir
+fi
+
+echo $PYTHONPATH
+
 python plotall.py \
     --product max_reflectivity \
     --nproc 1 \
     --fdate 20260202_00z \
     --map-type conus \
-    --base-path /discover/nobackup/jardizzo/EarthNow/plots \
+    --base-path /discover/nobackup/$USER/EarthNow/plots \
     --map-type goes_east_full_disk \
     --style satellite
 exit 0
