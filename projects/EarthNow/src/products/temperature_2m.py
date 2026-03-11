c6437044a1dfdb74e9dd3b93ad8fb0a31648b713
Hannah Zafar <hannah.zafar@nasa.gov>
2026-03-10 21:30:42 -0400

    Add black mistakes to test

--- projects/EarthNow/src/products/temperature_2m.py
+++ projects/EarthNow/src/products/temperature_2m.py
@@ -1,12 +1,10 @@
 """
 Temperature at 2-meters Product
 """
-
 import numpy as np
 import cartopy.crs as ccrs
 from matplotlib.colors import ListedColormap, BoundaryNorm
 from .registry import register
-
 # ------------------------------------------------------------------
 # Reflectivity colormap + levels (wxmaps-style)
 # ------------------------------------------------------------------
