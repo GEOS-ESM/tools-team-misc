0c3f30de93cae668cbcdfb043f48168a3f200690
Hannah Zafar <hannah.zafar@nasa.gov>
2026-03-24 17:06:30 -0400

    Setup plot_one.sh for input args

--- projects/EarthNow/bin/plot_one.sh
+++ projects/EarthNow/bin/plot_one.sh
@@ -2,6 +2,17 @@
 
 #NOTE: Running using the uv python config
 
+# Add input arg to run various tests
+# Check if exactly one argument is provided
+if [ -z "$1" ]; then
+    echo "Error: No argument provided."
+    echo "Usage: $0 {conus|global}"
+    exit 1
+fi
+
+# Convert the input to lowercase (to handle 'CONUS', 'Global', etc.)
+INPUT="${1,,}"
+
 bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Test uv setup works
@@ -11,15 +22,34 @@ bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # uv run tests/test_uv.py
 # 
 
-PYTHONPATH="$srcdir${PYTHONPATH:+:$PYTHONPATH}" \
-    python "$bindir/plotall.py" \
-    --product temperature_2m_EarthNow \
-    --nproc 1 \
-    --fdate 20260202_00z \
-    --pdate 20260202_1600 \
-    --map-type global \
-    --base-path /discover/nobackup/$USER/EarthNow/plots \
-    --style light \
-    --boundaries countries \
-    --boundaries states 
-exit 0
+# Test GLOBAL
+if [ "$INPUT" == "global" ]; then
+  uv run "$bindir/plotall.py" \
+      --product vorticity_heights_500mb_EarthNow \
+      --nproc 1 \
+      --fdate 20260202_00z \
+      --pdate 20260202_1600 \
+      --map-type global \
+      --base-path /discover/nobackup/$USER/EarthNow/plots \
+      --style dark 
+  exit 0
+
+# Test CONUS
+elif [ "$INPUT" == "conus" ]; then
+  uv run "$bindir/plotall.py" \
+      --product vorticity_heights_500mb_EarthNow \
+      --nproc 1 \
+      --fdate 20260202_00z \
+      --pdate 20260202_1600 \
+      --map-type conus \
+      --base-path /discover/nobackup/$USER/EarthNow/plots \
+      --style satellite
+  exit 0
+
+else
+  # Catch-all for invalid arguments
+  echo "Error: Invalid argument '$1'."
+  echo "Usage: $0 {conus|global}"
+  exit 1
+
+fi
