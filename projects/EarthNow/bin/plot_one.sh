59de878d7ddc61957c06207a29350f13d11f726d
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-07 15:19:51 -0400

    Change around bin plot file

--- projects/EarthNow/bin/plot_one.sh
+++ projects/EarthNow/bin/plot_one.sh
@@ -5,51 +5,36 @@
 # Add input arg to run various tests
 # Check if exactly one argument is provided
 if [ -z "$1" ]; then
-    echo "Error: No argument provided."
-    echo "Usage: $0 {conus|global}"
-    exit 1
+  echo "Error: No argument provided."
+  echo "Usage: $0 [conus|global]"
+  exit 1
 fi
 
 # Convert the input to lowercase (to handle 'CONUS', 'Global', etc.)
 INPUT="${1,,}"
+if [[ "$INPUT" != "conus" && "$INPUT" != "global" ]]; then
+  echo "Error: Invalid argument. Valid args: 'conus',  'global'."
+  exit 1
+fi
 
 bindir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Test uv setup works
-# script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# project_root="$(dirname "$script_dir")"
-# cd "$project_root"
-# uv run tests/test_uv.py
-# 
+# Set fdate/pdate variables
+# date="20260202"
+date="20260324"
+fdate=$date"_00z"
+pdate=$date"_0000"
 
-# Test GLOBAL
-if [ "$INPUT" == "global" ]; then
-  uv run "$bindir/plotall.py" \
-      --product vorticity_heights_500mb_EarthNow \
-      --nproc 1 \
-      --fdate 20260202_00z \
-      --pdate 20260202_1600 \
-      --map-type global \
-      --base-path /discover/nobackup/$USER/EarthNow/plots \
-      --style grey_topo
-  exit 0
+# Set product variable
+PRODUCT="vorticity_heights_500mb_EarthNow"
 
-# Test CONUS
-elif [ "$INPUT" == "conus" ]; then
-  uv run "$bindir/plotall.py" \
-      --product vorticity_heights_500mb_EarthNow \
-      --nproc 1 \
-      --fdate 20260202_00z \
-      --pdate 20260202_1600 \
-      --map-type conus \
-      --base-path /discover/nobackup/$USER/EarthNow/plots \
-      --style grey_topo
-  exit 0
-
-else
-  # Catch-all for invalid arguments
-  echo "Error: Invalid argument '$1'."
-  echo "Usage: $0 {conus|global}"
-  exit 1
-
-fi
+# Generate plots from args
+uv run "$bindir/plotall.py" \
+  --product "$PRODUCT" \
+  --nproc 1 \
+  --fdate $fdate \
+  --pdate $pdate \
+  --map-type "$INPUT" \
+  --base-path /discover/nobackup/"$USER"/EarthNow/plots \
+  --style grey_topo
+exit 0
