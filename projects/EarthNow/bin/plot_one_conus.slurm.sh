4618a52439ca986d9c687de93ad4c86bb0718f1b
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-17 12:41:49 -0400

    Revisions to SLURM scripts

--- projects/EarthNow/bin/plot_one_conus.slurm.sh
+++ projects/EarthNow/bin/plot_one_conus.slurm.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=global_vort_images
+#SBATCH --job-name=conus_vort_image_single
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.out
 #SBATCH --account=s1460
 #SBATCH --time=0:30:00
 #SBATCH --no-requeue
 
-./plot_one.sh global single
+./plot_one.sh conus single
+
+
+
 
