64e6f22ddca2a8fdbd1398a3f9268a2b3a701bc3
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-24 16:01:09 -0400

    Organization - move ignore files to scratch

--- projects/EarthNow/bin/mp4_wrapper.sh
+++ projects/EarthNow/bin/mp4_wrapper.sh
@@ -11,7 +11,7 @@ SLURM_JOB_ID=$2
 # Access SLURM log file and extract saved image paths to file
 LOG_FILE="${MAPTYPE}_vort_images-${SLURM_JOB_ID}.out"
 sync
-grep "Saved: " "$LOG_FILE" | awk '{print $2}' | sort -V | awk '{print "file \x27" $1 "\x27"}' > file_list.txt
+grep "Saved: " "$LOG_FILE" | awk '{print $2}' | sort -V | awk '{print "file \x27" $1 "\x27"}' > scratch/file_list.txt
 
 PLOTS_PATH=/discover/nobackup/"$USER"/EarthNow/plots/
 
@@ -19,5 +19,5 @@ OUTPUT_NAME="vorticity_${MAPTYPE}_SLURM-${SLURM_JOB_ID}.mp4"
 OUTPUT=$PLOTS_PATH$OUTPUT_NAME
 
 
-mp4_generator.sh -o "$OUTPUT" file_list.txt
+mp4_generator.sh -o "$OUTPUT" scratch/file_list.txt
 
