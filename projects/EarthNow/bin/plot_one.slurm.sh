2584399ceac00be66a35415c95eb898914722175
Hannah Zafar <hannah.zafar@nasa.gov>
2026-05-08 12:53:19 -0400

    Reorganized output files (checked SLURM works)

--- projects/EarthNow/bin/plot_one.slurm.sh
+++ projects/EarthNow/bin/plot_one.slurm.sh
@@ -4,10 +4,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --output=scratch/%x-%j.out
+#SBATCH --output=/discover/nobackup/%u/tools-team-misc/projects/EarthNow/output/%x-%j.out
 #SBATCH --account=s1460
 #SBATCH --time=0:30:00
 #SBATCH --no-requeue
 
-./plot_one.sh global greyblue 20260401_00z "winds_heights_250mb_EarthNow" all 20260401_0830z
+./plot_one.sh global grey_topo 20260508_00z "winds_heights_250mb_EarthNow" all
 
