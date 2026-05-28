dfa34d2b22bff5bd2e4829a6cf2aafa64cb2bd51
Hannah Zafar <hannah.zafar@nasa.gov>
2026-05-28 13:49:15 -0400

    Initial commit

--- projects/EarthNow/src/earthnow/products/EarthNow/__init__.py
+++ projects/EarthNow/src/earthnow/products/EarthNow/__init__.py
@@ -8,5 +8,4 @@ from . import winds_10m_EarthNow
 from . import vorticity_heights_500mb
 from . import winds_heights_250mb
 from . import rain_snow_accumulation_total
-
-# from . import max_reflectivity
+from . import max_reflectivity
