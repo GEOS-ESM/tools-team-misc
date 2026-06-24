b17073defe9525c2b1fa838799fee914b42078ac
Hannah Zafar <hannah.zafar@nasa.gov>
2026-06-24 13:17:28 -0400

    Comment out imports of GEOS_WxMap

--- projects/EarthNow/src/earthnow/products/__init__.py
+++ projects/EarthNow/src/earthnow/products/__init__.py
@@ -6,7 +6,8 @@ Product package for EarthNow/WxMaps
 from .registry import PRODUCTS
 
 # import modules so they self-register
-from . import GEOS_WxMaps
+# COMMENTED OUT BELOW - We don't need to load all the the GEOS_WxMaps functions because we don't use them and they slow down run times
+# from . import GEOS_WxMaps
 from . import EarthNow
 
 __all__ = ["PRODUCTS"]
