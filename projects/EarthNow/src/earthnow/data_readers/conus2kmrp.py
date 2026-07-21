c750fab396b3e5e75f9c77ab88f9532d0a9b6aed
Emily I. Bell <emily.bell@ssaihq.com>
2026-07-21 17:18:33 -0400

    Add missing variables

--- projects/EarthNow/src/earthnow/data_readers/conus2kmrp.py
+++ projects/EarthNow/src/earthnow/data_readers/conus2kmrp.py
@@ -15,6 +15,8 @@ CONUS2KMRP_VARS = VARIABLE_REGISTRY.resolve_many(
         "CAPE": "inst1_2d_asm_Nx",
         "RAIN": "inst1_2d_asm_Nx",
         "SNOW": "inst1_2d_asm_Nx",
+        "U10M": "inst1_2d_asm_Nx",
+        "V10M": "inst1_2d_asm_Nx",
         "U250": "inst1_2d_asm_Nx",
         "V250": "inst1_2d_asm_Nx",
         "SLP": "inst1_2d_asm_Nx",
