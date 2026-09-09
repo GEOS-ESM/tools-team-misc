f46eb56883887dbdc2548f26caa74be6f9608911
Emily I. Bell <emily.bell@ssaihq.com>
2026-09-09 10:43:21 -0400

    TBRB10RG to variables.yaml and data readers

--- projects/EarthNow/src/earthnow/data_readers/conus2kmfc_lcc.py
+++ projects/EarthNow/src/earthnow/data_readers/conus2kmfc_lcc.py
@@ -38,6 +38,7 @@ CONUS2KMFC_LCC_VARS = VARIABLE_REGISTRY.resolve_many(
         "OSRB11RG": "hwt_15mn_slv_LCC",
         "OSRB10RG": "hwt_15mn_slv_LCC",
         "OSRB09RG": "hwt_15mn_slv_LCC",
+        "TBRB10RG": "hwt_15mn_slv_LCC",
     }
 )
 
